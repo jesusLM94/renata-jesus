6d141c2373117dce31af960efada86bbfc581652
Jesús Lizárraga <jesuslizarraga@Jesuss-MacBook-Pro.local>
2021-12-02 21:34:49 -0600

    Add new hero image and change navigation to scroll tto section instead of having links

--- src/components/header/navigation.js
+++ src/components/header/navigation.js
@@ -1,20 +1,27 @@
 import React from 'react'
 
 const Navigation = (props) => {
-  const { className, isOpen } = props
+  const { className } = props
+
+  const executeScroll = (e, id) => {
+    const element = document.getElementById(id);
+    e.preventDefault();
+    element?.scrollIntoView({behavior: 'smooth'});
+  }
+
   return (
     <div className={className}>
       <nav className="navbar-nav flex flex-col sm:flex-row">
-        <a className="md:px-5" href="#home">
+        <a onClick={(e) => executeScroll(e,'home')} className="md:px-5" href="/">
           Inicio
         </a>
-        <a className="md:px-5" href="#when-and-where">
+        <a onClick={(e) => executeScroll(e,'when-and-where')} className="md:px-5" href="/">
           ¿Dónde y Cúando?
         </a>
-        <a className="md:px-5" href="#gift-table">
+        <a onClick={(e) => executeScroll(e,'gift-table')} className="md:px-5" href="/">
           Mesa de Regalos
         </a>
-        <a className="md:px-5" href="#confirmation">
+        <a onClick={(e) => executeScroll(e,'confirmation')} className="md:px-5" href="/">
           Confirmar Asistencia
         </a>
       </nav>
