7767824a217bc30ffa40d4322793a365273ae0bc
Jesús Lizárraga <jesuslizarraga@Jesuss-MacBook-Pro.local>
2021-12-02 21:35:36 -0600

    Update guestlist

--- src/helpers/guestList.js
+++ src/helpers/guestList.js
@@ -1,22 +1,390 @@
 export const guestList = Object.freeze({
-  renata: {
+  carlos: {
     id: '1',
-    name: 'Renata Salazar Villaseñor',
-    tickets: '2',
+    name: 'Carlos René Salazar Hernández',
+    tickets: '1',
   },
   laura: {
     id: '2',
-    name: 'Laura Villaseñor Aguirre',
-    tickets: '2',
+    name: 'Laura Isabel Villaseñor Aguirre',
+    tickets: '1',
   },
   nelva: {
     id: '3',
     name: 'Nelva Montelongo Galván',
-    tickets: '2',
+    tickets: '1',
   },
-  sofia: {
+  jesus: {
     id: '4',
-    name: 'Sofia Salazar Villaseñor',
+    name: 'Jesús Lizárraga Calleros',
+    tickets: '1',
+  },
+  cueva: {
+    id: '5',
+    name: 'Familia Cueva Salazar',
     tickets: '3',
   },
+  karla: {
+    id: '6',
+    name: 'Karla Priscilla Salazar Villaseñor',
+    tickets: '2',
+  },
+  ortega: {
+    id: '7',
+    name: 'Familia Ortega Villaseñor',
+    tickets: '2',
+  },
+  josefina: {
+    id: '8',
+    name: 'Srita. Josefina Villaseñor Aguirre',
+    tickets: '1',
+  },
+  bertha: {
+    id: '9',
+    name: 'Srita. Bertha Guadalupe Villaseñor Aguirre',
+    tickets: '1',
+  },
+  carmen: {
+    id: '10',
+    name: 'Srita. María del Carmen Villaseñor Aguirre',
+    tickets: '1',
+  },
+  villaseñorvera: {
+    id: '11',
+    name: 'Familia Villaseñor Vera',
+    tickets: '2',
+  },
+  villaseñormoreno: {
+    id: '12',
+    name: 'Familia Villaseñor Moreno',
+    tickets: '2',
+  },
+  leonila: {
+    id: '13',
+    name: 'Sra. Leonila Villaseñor Aguirre',
+    tickets: '2',
+  },
+  gomezgonzalez: {
+    id: '14',
+    name: 'Familia Gómez González',
+    tickets: '8',
+  },
+  gomezmarrufo: {
+    id: '15',
+    name: 'Familia Gómez Marrufo',
+    tickets: '2',
+  },
+  ruizavalos: {
+    id: '16',
+    name: 'Familia Ruiz Ávalos',
+    tickets: '2',
+  },
+  hernandezavalos: {
+    id: '17',
+    name: 'Familia Hernández Ávalos',
+    tickets: '2',
+  },
+  sofi: {
+    id: '18',
+    name: 'Sofía Izquierdo Márquez',
+    tickets: '2',
+  },
+  garibay: {
+    id: '19',
+    name: 'Familia Garibay',
+    tickets: '2',
+  },
+  fa: {
+    id: '20',
+    name: 'Fá Elías',
+    tickets: '2',
+  },
+  lopezlases: {
+    id: '21',
+    name: 'Familia López Lases',
+    tickets: '2',
+  },
+  andrea: {
+    id: '22',
+    name: 'Andrea Flores Gamboa',
+    tickets: '2',
+  },
+  silvia: {
+    id: '23',
+    name: 'Silvia Heiras Jaime',
+    tickets: '2',
+  },
+  ivette: {
+    id: '24',
+    name: 'Ivette Flores Luján',
+    tickets: '2',
+  },
+  juanpe: {
+    id: '25',
+    name: 'Juan Pedro Leyva Fernández',
+    tickets: '1',
+  },
+  pau: {
+    id: '26',
+    name: 'Paulina López Romero',
+    tickets: '2',
+  },
+  dario: {
+    id: '27',
+    name: 'Darío Cosío',
+    tickets: '2',
+  },
+  vale: {
+    id: '28',
+    name: 'Valeria Peña',
+    tickets: '1',
+  },
+  magove: {
+    id: '29',
+    name: 'Familia Alvarez Gómez',
+    tickets: '2',
+  },
+  jacob: {
+    id: '30',
+    name: 'Jacob Janani Pantoja Sánchez',
+    tickets: '2',
+  },
+  dolores: {
+    id: '31',
+    name: 'Sra. Dolores Galván',
+    tickets: '1',
+  },
+  angulolizarraga: {
+    id: '32',
+    name: 'Familia Angúlo Lizárraga',
+    tickets: '2',
+  },
+  rodriguez: {
+    id: '33',
+    name: 'Familia Rodríguez Montelongo',
+    tickets: '4',
+  },
+  aracely: {
+    id: '34',
+    name: 'Sra. Aracely Montelongo Galván',
+    tickets: '1',
+  },
+  // Caro & Adrian
+  rodriguezgarcia: {
+    id: '35',
+    name: 'Familia Rodríguez García',
+    tickets: '2',
+  },
+  garciaolea: {
+    id: '36',
+    name: 'Familia García Olea',
+    tickets: '2',
+  },
+  // Pepe & Ana
+  montelongogarcia: {
+    id: '37',
+    name: 'Familia Montelongo García',
+    tickets: '4',
+  },
+  arellanomontelongo: {
+    id: '38',
+    name: 'Familia Arellano Montelongo',
+    tickets: '4',
+  },
+  galaviz: {
+    id: '39',
+    name: 'Familia Galaviz Montelongo',
+    tickets: '5',
+  },
+  peña: {
+    id: '40',
+    name: 'Familia Peña Montelongo',
+    tickets: '4',
+  },
+  cabanillas: {
+    id: '41',
+    name: 'Familia Cabanillas Lizárraga',
+    tickets: '3',
+  },
+  // Juan & Sara
+  lizarragaromero: {
+    id: '42',
+    name: 'Familia Lizárraga Romero',
+    tickets: '2',
+  },
+  osunalizarraga: {
+    id: '43',
+    name: 'Familia Osuna Lizárraga',
+    tickets: '3',
+  },
+  // Idania & Alfredo
+  lizarragacardenas: {
+    id: '44',
+    name: 'Familia Lizárraga Cárdenas',
+    tickets: '2',
+  },
+  dalia: {
+    id: '45',
+    name: 'Sra. Dalia Lizárraga Calleros',
+    tickets: '2',
+  },
+  teresa: {
+    id: '46',
+    name: 'Sra. Teresa Lizárraga Calleros',
+    tickets: '2',
+  },
+  hilda: {
+    id: '47',
+    name: 'Sra. Hilda Calleros',
+    tickets: '1',
+  },
+  chinchillas: {
+    id: '48',
+    name: 'Las Chinchillas',
+    tickets: '2',
+  },
+  mitzi: {
+    id: '49',
+    name: 'Mitzi Lizárraga Romero',
+    tickets: '2',
+  },
+  migue: {
+    id: '50',
+    name: 'Miguel Valenzuela',
+    tickets: '2',
+  },
+  esteban: {
+    id: '51',
+    name: 'Esteban Meneses',
+    tickets: '2',
+  },
+  rodrigo: {
+    id: '52',
+    name: 'Rodrigo Meneses',
+    tickets: '1',
+  },
+  alejandro: {
+    id: '53',
+    name: 'Alejandro Calderón',
+    tickets: '2',
+  },
+  quique: {
+    id: '54',
+    name: 'Luis Enrique González',
+    tickets: '1',
+  },
+  nacho: {
+    id: '55',
+    name: 'Nacho Luchador',
+    tickets: '1',
+  },
+  orlando: {
+    id: '56',
+    name: 'Orlando Ramírez',
+    tickets: '2',
+  },
+  // Faltan datos (Apellido Ana Karen)
+  primo: {
+    id: '57',
+    name: 'Familia Calderón P',
+    tickets: '2',
+  },
+  //Faltan datos (confirmar rubicel)
+  rafa: {
+    id: '58',
+    name: 'Familia Velázques Rubicel',
+    tickets: '2',
+  },
+  pepe: {
+    id: '59',
+    name: 'José Carlos Aispuro',
+    tickets: '2',
+  },
+  barraza: {
+    id: '60',
+    name: 'Fernando Barraza Quintero',
+    tickets: '2',
+  },
+  lasse: {
+    id: '61',
+    name: 'Lasse De Graff',
+    tickets: '2',
+  },
+  malene: {
+    id: '62',
+    name: 'Malene Haugehansen',
+    tickets: '2',
+  },
+  meghan: {
+    id: '63',
+    name: 'Meghan Gruenewald',
+    tickets: '1',
+  },
+  estefy: {
+    id: '64',
+    name: 'Estefania Benavente',
+    tickets: '2',
+  },
+  fabian: {
+    id: '65',
+    name: 'Fabian Kruckenhauser',
+    tickets: '1',
+  },
+  landon: {
+    id: '66',
+    name: 'Landon Butter',
+    tickets: '1',
+  },
+  // Faltan datos, confirmar apellido
+  dino: {
+    id: '67',
+    name: 'Dinorah Abrego',
+    tickets: '1',
+  },
+  farid: {
+    id: '68',
+    name: 'Farid Izar',
+    tickets: '1',
+  },
+  paulina: {
+    id: '69',
+    name: 'Paulina Ortega',
+    tickets: '2',
+  },
+  nick: {
+    id: '70',
+    name: 'Nicholas Chai',
+    tickets: '1',
+  },
+  fede: {
+    id: '70',
+    name: 'Federico Gómez',
+    tickets: '2',
+  },
+  // Faltan datos (apellido)
+  christian: {
+    id: '70',
+    name: 'Christian',
+    tickets: '2',
+  },
+  anakaren: {
+    id: "71",
+    name: "Ana Karen Ortega Villaseñor",
+    tickets: "2",
+  },
+  extras: {
+    id: "72",
+    name: "Extras",
+    tickets: "5",
+  },
 })
+
+export const numberOfGuests = (guestList) => {
+  // count number of tickets
+  return guestList.reduce((sum, guest) => {
+    sum = sum + parseInt(guest.tickets)
+
+    return sum
+  }, 0)
+}
+
