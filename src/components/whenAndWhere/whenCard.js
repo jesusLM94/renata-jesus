19e8a1d23f3d8f8edd013c599f60d98daf06f30c
Jesús Lizárraga <jesuslizarraga@Jesuss-MacBook-Pro.local>
2021-12-04 16:45:38 -0600

    Add padding to small devices

--- src/components/whenAndWhere/whenCard.js
+++ src/components/whenAndWhere/whenCard.js
@@ -6,7 +6,7 @@ const Card = (props) => {
 
   return (
     <div className="m-5">
-      <figure class="bg-primary rounded-xl sm:p-8">
+      <figure class="bg-primary rounded-xl p-3 sm:p-8">
         <h5 className="text-xl flex flex-wrap justify-center mb-2 font-bold">{title}</h5>
         <p className="text-lg flex flex-wrap justify-center mb-2">{text}</p>
         <p className="text-lg flex flex-wrap justify-center mb-5">{time}</p>
