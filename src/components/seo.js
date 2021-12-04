041f95853e0d2930f5e253a26c35cc5f45f0db0f
Jesús Lizárraga <jesuslizarraga@Jesuss-MacBook-Pro.local>
2021-12-04 15:34:54 -0600

    Add details for SEO component

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,21 +1,14 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { Helmet } from "react-helmet"
-import { useLocation } from "@reach/router"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react'
+import { Helmet } from 'react-helmet'
+import { useLocation } from '@reach/router'
+import { useStaticQuery, graphql } from 'gatsby'
 
-const SEO = ({ title, description, image, article }) => {
+const SEO = ({ title = null, description = null, image = null }) => {
   const { pathname } = useLocation()
   const { site } = useStaticQuery(query)
 
-  const {
-    defaultTitle,
-    titleTemplate,
-    defaultDescription,
-    siteUrl,
-    defaultImage,
-    twitterUsername,
-  } = site.siteMetadata
+  const { defaultTitle, titleTemplate, defaultDescription, siteUrl, defaultImage } =
+    site.siteMetadata
 
   const seo = {
     title: title || defaultTitle,
@@ -31,27 +24,15 @@ const SEO = ({ title, description, image, article }) => {
 
       {seo.url && <meta property="og:url" content={seo.url} />}
 
-      {(article ? true : null) && <meta property="og:type" content="article" />}
-
       {seo.title && <meta property="og:title" content={seo.title} />}
 
-      {seo.description && (
-        <meta property="og:description" content={seo.description} />
-      )}
+      {seo.description && <meta property="og:description" content={seo.description} />}
 
       {seo.image && <meta property="og:image" content={seo.image} />}
 
-      <meta name="twitter:card" content="summary_large_image" />
-
-      {twitterUsername && (
-        <meta name="twitter:creator" content={twitterUsername} />
-      )}
-
       {seo.title && <meta name="twitter:title" content={seo.title} />}
 
-      {seo.description && (
-        <meta name="twitter:description" content={seo.description} />
-      )}
+      {seo.description && <meta name="twitter:description" content={seo.description} />}
 
       {seo.image && <meta name="twitter:image" content={seo.image} />}
     </Helmet>
@@ -59,20 +40,6 @@ const SEO = ({ title, description, image, article }) => {
 }
 
 export default SEO
-
-SEO.propTypes = {
-  title: PropTypes.string,
-  description: PropTypes.string,
-  image: PropTypes.string,
-  article: PropTypes.bool,
-}
-
-SEO.defaultProps = {
-  title: null,
-  description: null,
-  image: null,
-  article: false,
-}
 
 const query = graphql`
   query SEO {
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,8 @@ import GiftTable from '../components/giftTable'
 import Footer from '../components/footer'
 import Invitation from '../components/invitation'
 import Welcome from '../components/welcome'
-import '../assets/styles/index.scss'
 import SEO from '../components/seo'
+import '../assets/styles/index.scss'
 
 const IndexPage = (props) => {
   const inviteId = props.location?.search?.slice(12)
@@ -16,6 +16,7 @@ const IndexPage = (props) => {
   return (
     <main className="base-styles">
       <title>Home Page</title>
+      <SEO />
       <Header />
       <HeroImage />
       <Welcome />
@@ -24,7 +25,6 @@ const IndexPage = (props) => {
       <Dresscode />
       <GiftTable />
       <Footer />
-      <SEO />
     </main>
   )
 }
