2cee7e3484e30bd238b9931e9d858333785f3bdd
Jesús Lizárraga <jesuslizarraga@Jesuss-MacBook-Pro.local>
2021-12-08 11:54:24 -0600

    Add different mobile markup for parents part

--- src/components/welcome/index.js
+++ src/components/welcome/index.js
@@ -8,20 +8,29 @@ const Welcome = () => {
           Con la bendición de Dios y en compañía de nuestros padres
         </p>
 
-        <div className="flex flex-col items-center justify-center my-5 sm:my-10">
-          <div className="flex">
-            <p className="mt-3 sm:mt-2 mr-3 sm:mr-10 text-right">Laura Isabel Villaseñor Aguirre</p>
-            <p className="mt-3 sm:mt-2 text-left">Nelva Gpe.Montelongo Galván</p>
+        <div className="hidden sm:flex items-center justify-center my-5 sm:my-10">
+          <div className="flex text-right flex-col mr-1.5 sm:mr-5">
+            <p className="mt-3 sm:mt-2">Laura Isabel Villaseñor Aguirre</p>
+            <p className="mt-3 sm:mt-2">Carlos René Salazar Hernández</p>
           </div>
 
-          <div className="flex">
-            <p className="mt-3 sm:mt-2 mr-3 sm:mr-10 text-right">Carlos René Salazar Hernández</p>
-            <p className="mt-3 sm:mt-2 text-left">Jesús Manuel Lizárraga Calleros</p>
+          <div className="flex text-left flex-col ml-1.5 sm:ml-5">
+            <p className="mt-3 sm:mt-2">Nelva Gpe. Montelongo Galván</p>
+            <p className="mt-3 sm:mt-2">Jesús Manuel Lizárraga Calleros</p>
           </div>
         </div>
-        <p className="my-10">Y nuestros padrinos Ana García Sauceda y José Montelongo Galván</p>
 
-        <p className="mt-5 sm:mt-10">Uniremos nuestras vidas en matrimonio</p>
+        <div className="flex sm:hidden flex-col items-center justify-center my-5 sm:my-10">
+          <p>Laura Isabel Villaseñor Aguirre</p>
+          <p className="mt-3">Carlos René Salazar Hernández</p>
+          <p className="mt-3">Nelva Guadalupe Montelongo Galván</p>
+          <p className="mt-3">Jesús Manuel Lizárraga Calleros</p>
+        </div>
+
+        <p className="mt-7 sm:mt-10">Y nuestros padrinos</p>
+        <p className="">Ana García Sauceda y José Montelongo Galván</p>
+
+        <p className="mt-7 sm:mt-10">Uniremos nuestras vidas en matrimonio</p>
       </div>
       <h3 className="hidden sm:flex heading-text justify-center mt-5">
         Renata Salazar & Jesús Lizárraga
