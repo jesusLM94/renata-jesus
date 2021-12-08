fd57e168fcf47a9fac960678f4413c6b764cf3be
Jesús Lizárraga <jesuslizarraga@Jesuss-MacBook-Pro.local>
2021-12-07 22:06:59 -0600

    Add footer page

---
+++ src/components/footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './index.scss'
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="w-full h-full bg-white top-0 left-0 absolute opacity-50" />
+      <p className="flex justify-center items-end h-full text-4xl sm:text-6xl pb-12 sm:pb-10 dancing-script">
+        ¡Gracias por acompañarnos!
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import Confirmation from '../components/confirmation'
 import Invitation from '../components/invitation'
 import Welcome from '../components/welcome'
 import Timeline from '../components/timeline'
+import Footer from '../components/footer'
 import Seo from '../components/seo'
 import { guestList } from '../helpers/guestList'
 import smoothscroll from 'smoothscroll-polyfill'
@@ -33,6 +34,7 @@ const IndexPage = (props) => {
       <Timeline />
       <GiftTable />
       <Confirmation person={person} />
+      <Footer />
     </main>
   )
 }
