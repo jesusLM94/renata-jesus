10912c50eae31063a29897b1e545e17b84b070cc
Jesús Lizárraga <jesuslizarraga@Jesuss-MacBook-Pro.local>
2021-12-07 21:32:54 -0600

    Add polyfill for smooth scrolling on safari

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,21 @@ import HeroImage from '../components/heroImage'
 import WhenAndWhere from '../components/whenAndWhere'
 import Dresscode from '../components/dressCode'
 import GiftTable from '../components/giftTable'
-import Footer from '../components/footer'
+import Confirmation from '../components/confirmation'
 import Invitation from '../components/invitation'
 import Welcome from '../components/welcome'
 import Timeline from '../components/timeline'
 import Seo from '../components/seo'
+import { guestList } from '../helpers/guestList'
+import smoothscroll from 'smoothscroll-polyfill'
 import '../assets/styles/index.scss'
 
 const IndexPage = (props) => {
   const inviteId = props.location?.search?.slice(12)
+  const person = guestList[inviteId]
+
+  // kick off the polyfill for smooth scrolling
+  smoothscroll.polyfill()
 
   return (
     <main className="base-styles">
@@ -21,12 +27,12 @@ const IndexPage = (props) => {
       <Header />
       <HeroImage />
       <Welcome />
-      <Invitation inviteId={inviteId} />
+      <Invitation person={person} />
       <WhenAndWhere />
       <Dresscode />
       <Timeline />
       <GiftTable />
-      <Footer />
+      <Confirmation person={person} />
     </main>
   )
 }
